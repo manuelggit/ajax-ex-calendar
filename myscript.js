c1a0d0de2b48f780c723694c7e73f09988050a8f
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-20 16:52:31 +0100

    Variabile numero dei giorni a gennaio 2018

--- myscript.js
+++ myscript.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
-  var dataOggi = moment().format('DD MMMM YYYY');
-  console.log(dataOggi);
+  var dataOggi = moment().format('DD MMMM');
+  console.log('oggi è il:', dataOggi);
+
+  var giorniGen2018 = moment("2018-01").daysInMonth();
+  console.log('Gennaio ha ' + giorniGen2018 + ' giorni');
 
 });
