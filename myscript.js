84a43f94e0f9a89c6816a3276713d34cf1499e22
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-21 13:07:07 +0100

    Creo var dataGiorno per creare date formattate come nell'api

--- myscript.js
+++ myscript.js
@@ -10,24 +10,30 @@ $(document).ready(function() {
     method:'GET',
     success: function (festeapi) {
       var festivita = festeapi.response;
-      console.log(festa); // è un array di festivita
+      console.log(festivita); // è un array di festivita
 
       var giorniGen2018 = moment("2018-01").daysInMonth();
-      console.log('Gennaio ha ' + giorniGen2018 + ' giorni');
+      // console.log('Gennaio ha ' + giorniGen2018 + ' giorni');
+
+      for (var j = 0; j < festivita.length; j++) { // attraverso l'array di festivita
+        var festaNome = festivita[j].name
+        console.log(festaNome);
+        var festaData = festivita[j].date
+        console.log(festaData);
+      }
 
       for (var i = 1; i <= giorniGen2018; i++) {
 
         var giorno = moment().date(i).month(0).format('DD MMMM')
-        console.log(giorno);
+        var dataGiorno = moment('2018-01-'+i, 'YYYY-MM-D').format('YYYY-MM-DD'); // lo formatto com'è nell'api
+        console.log(giorno, dataGiorno);
         $('#gennaio').append("<div>" + giorno + "</div>");
 
-      }
-
-      for (var j = 0; j < festivita.length; j++) { // attraverso l'array di festivita
-        var festa = festivita[j].name
-        console.log(festa);
 
       }
+
+
+
 
     }
 
