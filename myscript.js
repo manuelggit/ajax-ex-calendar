2e7fc0324ae87ab501723adfa52ef998383b6445
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-21 09:31:38 +0100

    Console.log di festa

--- myscript.js
+++ myscript.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
     url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
     method:'GET',
     success: function (festeapi) {
-      var festa = festeapi.response;
-      console.log(festa);
+      var festivita = festeapi.response;
+      console.log(festa); // è un array di festivita
 
       var giorniGen2018 = moment("2018-01").daysInMonth();
       console.log('Gennaio ha ' + giorniGen2018 + ' giorni');
@@ -19,7 +19,14 @@ $(document).ready(function() {
 
         var giorno = moment().date(i).month(0).format('DD MMMM')
         console.log(giorno);
-        $('#gennaio').append("<div>" + giorno + "</div>")
+        $('#gennaio').append("<div>" + giorno + "</div>");
+
+      }
+
+      for (var j = 0; j < festivita.length; j++) { // attraverso l'array di festivita
+        var festa = festivita[j].name
+        console.log(festa);
+
       }
 
     }
