4085d0e868e62004f2d0ecce0361519f304bcd44
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-21 09:17:01 +0100

    Metto tutto all'interno della chiamata ajax che va a buon fine

--- myscript.js
+++ myscript.js
@@ -1,17 +1,9 @@
 $(document).ready(function() {
 
-  var dataOggi = moment().format('DD MMMM');
-  console.log('oggi è il:', dataOggi);
+  // var dataOggi = moment().format('DD MMMM');
+  // console.log('oggi è il:', dataOggi);
 
-  var giorniGen2018 = moment("2018-01").daysInMonth();
-  console.log('Gennaio ha ' + giorniGen2018 + ' giorni');
 
-  for (var i = 1; i <= giorniGen2018; i++) {
-
-    var giorno = moment().date(i).month(0).format('DD MMMM')
-    console.log(giorno);
-    $('#gennaio').append("<div>" + giorno + "</div>")
-  }
 
   $.ajax ({
     url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
@@ -19,8 +11,20 @@ $(document).ready(function() {
     success: function (festeapi) {
       var festa = festeapi.response;
       console.log(festa);
+
+      var giorniGen2018 = moment("2018-01").daysInMonth();
+      console.log('Gennaio ha ' + giorniGen2018 + ' giorni');
+
+      for (var i = 1; i <= giorniGen2018; i++) {
+
+        var giorno = moment().date(i).month(0).format('DD MMMM')
+        console.log(giorno);
+        $('#gennaio').append("<div>" + giorno + "</div>")
+      }
+
     }
-  })
+
+  });
 
 
 });
